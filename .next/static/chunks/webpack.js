90cd220bfa15aac0db635702b3db57046209f4c5
Rehana-2427 <shaikrehana248@gmail.com>
2026-06-23 14:25:25 +0530

    update pkg file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2150f53ac9496c70"; }
+/******/ 		__webpack_require__.h = function() { return "3659fe3283b00a4d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
