2a3884321c9c7b807e878b724da1f32bd8e8c90f
Rehana-2427 <shaikrehana248@gmail.com>
2026-07-01 15:21:56 +0530

    commit changes updated next env d.ts file updated css things

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,23 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.module.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.module.scss" {
+  const content: { [className: string]: string };
+  export default content;
+}
